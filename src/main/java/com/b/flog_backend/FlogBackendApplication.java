8be755d3c02445210506bc7f6a20c6b3c8a63f91
gijeong9266 <gijeong9266@gmail.com>
2025-07-21 09:33:06 +0900

    initial commit

---
+++ src/main/java/com/b/flog_backend/FlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.b.flog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlogBackendApplication.class, args);
+	}
+
+}
