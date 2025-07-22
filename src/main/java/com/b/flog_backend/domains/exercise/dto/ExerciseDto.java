92de3c729910322332bc0ec3b262464d74998884
haemin <goalsgoals0417@gmail.com>
2025-07-23 08:44:28 +0900

    dto수정

--- src/main/java/com/b/flog_backend/domains/exercise/dto/ExerciseDto.java
+++ src/main/java/com/b/flog_backend/domains/exercise/dto/ExerciseDto.java
@@ -1,5 +1,6 @@
 package com.b.flog_backend.domains.exercise.dto;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import lombok.Data;
@@ -7,9 +8,11 @@ import lombok.Data;
 @Data
 public class ExerciseDto {
     private int id;
-    private int routineId;
     private int userId;
+    private int routineId;
     private String sector;
     private String exerciseName;
+    private LocalDate logDate;
+    private boolean isLoggedExercise;
     private LocalDateTime createdAt;
 }
